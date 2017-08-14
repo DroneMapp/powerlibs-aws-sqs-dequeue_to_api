8af7599d32c47709ec2c9a1db683cdfba9c2165b
cleber <cleber@dronemapp.com>
2017-08-14 14:44:06 -0300

    first draft

---
+++ bin/run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+RET=0
+
+pip uninstall -y $(basename $PWD) || echo "Could not uninstall."
+pip install -U "git+file://$PWD" --no-cache-dir
+mv powerlibs x
+PYTHONPATH=. py.test tests/ || RET=$?
+mv x powerlibs
+
+exit $RET
